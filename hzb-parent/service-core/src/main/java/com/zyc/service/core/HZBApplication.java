f3a850ecc81fa9a4c7b8ad742c1401e76bf2a63a
diLi <3211665098@qq.com>
2024-08-28 18:34:14 +0800

    数据字典相关功能完善

--- hzb-parent/service-core/src/main/java/com/zyc/service/core/HZBApplication.java
+++ hzb-parent/service-core/src/main/java/com/zyc/service/core/HZBApplication.java
@@ -2,8 +2,7 @@ package com.zyc.service.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 /**
  * 作者:ZYC
